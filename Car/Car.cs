bf1e9fd2dbb2fe9d2170a710c2f67e15683c2f1d
fhs15 <39727804+fhs15@users.noreply.github.com>
2022-03-09 13:32:08 +0100

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        private string _model;
+        private int _price;
+        private string _licensePlate;
+        public int Id { get; set; }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException();
+                if (value.Length < 4) throw new ArgumentOutOfRangeException();
+                _model = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value == null) throw new ArgumentNullException();
+                if (value.Length < 2 || value.Length > 7) throw new ArgumentOutOfRangeException();
+                _licensePlate = value;
+            }
+        }
+
+        public Car()
+        {
+
+        }
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+    }
+}
---
+++ CarTests/CarTests.cs
@@ -0,0 +1,77 @@
+﻿using Cars;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void CarTestModel()
+        {
+            Car car = new Car();
+
+            car.Model = "Mustang";
+            Assert.AreEqual("Mustang", car.Model);
+
+            Assert.ThrowsException<ArgumentNullException>((() => car.Model = null));
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.Model = "Ab"));
+        }
+
+        [TestMethod()]
+        public void CarTestPrice()
+        {
+            Car car = new Car();
+
+            car.Price = 100;
+            Assert.AreEqual(100, car.Price);
+
+            car.Price = 1;
+            Assert.AreEqual(1, car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.Price = 0));
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.Price = -1));
+        }
+
+        [TestMethod()]
+        public void CarTestLicensePlate()
+        {
+            Car car = new Car();
+
+            car.LicensePlate = "AB9432";
+            Assert.AreEqual("AB9432", car.LicensePlate);
+
+
+            car.LicensePlate = "12";
+            Assert.AreEqual("12", car.LicensePlate);
+            car.LicensePlate = "1234567";
+            Assert.AreEqual("1234567", car.LicensePlate);
+            Assert.ThrowsException<ArgumentNullException>((() => car.LicensePlate = null));
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.LicensePlate = "A"));
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => car.LicensePlate = "12345678"));
+        }
+
+        [TestMethod()]
+        public void CarTestConstructor()
+        {
+            Car car = new Car(1, "Mustang", 100, "AB94");
+
+            Assert.AreEqual(1, car.Id);
+            Assert.AreEqual("Mustang", car.Model);
+            Assert.AreEqual(100, car.Price);
+            Assert.AreEqual("AB94", car.LicensePlate);
+
+            Assert.ThrowsException<ArgumentNullException>(() => new Car(1, null, 100, "AB94"));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Car(1, "123", 100, "AB94"));
+
+            Assert.ThrowsException<ArgumentNullException>(() => new Car(1, "Mustang", 100, null));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Car(1, "Mustang", 100, "1"));
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Car(1, "Mustang", 0, "123"));
+        }
+    }
+}
